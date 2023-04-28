c6f13ed37ac5414afa404225cf415a4a7ada578d
Mayank Kumar Roy <mayankkumar27sept.mk@gmail.com>
2023-04-28 17:49:46 +0530

    Add files via upload

---
+++ simpleloginand signup system.py
@@ -0,0 +1,15 @@
+print("Create account now")
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+print("Your account has been created sucessfully")
+print("Login now!")
+
+username2 = input("Enter username: ")
+password2 = input("Enter password: ")
+
+if username == username2 and password == password2:
+    print(" login in sucessflly")
+
+else:
+    print("Invalid credentials")
